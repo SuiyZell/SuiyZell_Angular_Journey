8a67b390877edd7612cfa5c503313271472a718d
suiyunzhi-kdgc <xsui.yunzhi@contract.isid.co.jp>
2023-06-07 16:04:19 +0800

    实现获取运费功能

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {Injectable} from '@angular/core';
 import {Product} from "./products";
 
@@ -21,6 +22,11 @@ export class CartService {
         return this.items
     }
 
-    constructor() {
+    getShippingPrices(){
+        return this.http.get<{type:string,price:number}[]>
+        ('/assets/shipping.json')
+    }
+
+    constructor(private http: HttpClient) {
     }
 }
