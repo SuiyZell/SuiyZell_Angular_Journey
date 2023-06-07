df59ac9de96a77892547d4e5035f38747ba24f10
suiyunzhi-kdgc <xsui.yunzhi@contract.isid.co.jp>
2023-06-07 14:33:07 +0800

    根据不同的产品的id跳转到产品详情并显示不同的信息

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+import {Product, products} from "../products";
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
 
+  product: Product | undefined;
+
+  constructor(private route:ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    //获取product id，从current route中
+    const routeParams = this.route.snapshot.paramMap
+    const productIdFromRoute = Number(routeParams.get('productId'))
+
+    //Find the product that correspond with the id in route
+    this.product = products.find(product =>product.id===productIdFromRoute)
+  }
 }
