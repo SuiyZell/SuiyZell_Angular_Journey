cc9ab19cf24dcc2a8b62528848e02189a8b65475
suiyunzhi-kdgc <xsui.yunzhi@contract.isid.co.jp>
2023-06-07 15:32:00 +0800

    实现将商品添加到购物车中的功能

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,26 +1,34 @@
-import { Component,OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 import {Product, products} from "../products";
+import {CartService} from "../cart.service";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+    selector: 'app-product-details',
+    templateUrl: './product-details.component.html',
+    styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
 
-  product: Product | undefined;
+    product: Product | undefined;
 
-  constructor(private route:ActivatedRoute) {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private cartService: CartService
+    ) { }
 
-  ngOnInit(): void {
-    //获取product id，从current route中
-    const routeParams = this.route.snapshot.paramMap
-    const productIdFromRoute = Number(routeParams.get('productId'))
+    addToCart(product:Product){
+        this.cartService.addToCart(product);
+        window.alert('你的商品已经成功添加到购物车中！')
+    }
 
-    //Find the product that correspond with the id in route
-    this.product = products.find(product =>product.id===productIdFromRoute)
-  }
+    ngOnInit(): void {
+        //获取product id，从current route中
+        const routeParams = this.route.snapshot.paramMap
+        const productIdFromRoute = Number(routeParams.get('productId'))
+
+        //Find the product that correspond with the id in route
+        this.product = products.find(product => product.id === productIdFromRoute)
+    }
 }
